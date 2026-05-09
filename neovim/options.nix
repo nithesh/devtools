1a9b0b6c1d3d1d9345de780e83b8788c82297020
Nithesh Mittapally <nithesh@mittapally.com>
2026-05-08 23:08:04 -0400

    feat(neovim): rebalance diff splits on editor resize

--- neovim/options.nix
+++ neovim/options.nix
@@ -17,6 +17,24 @@
       termguicolors = true;
     };
 
+    extraConfigLua = ''
+      vim.api.nvim_create_autocmd("VimResized", {
+        callback = function()
+          local has_diff = false
+          for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.wo[win].diff then
+              has_diff = true
+              break
+            end
+          end
+
+          if has_diff then
+            vim.cmd("wincmd =")
+          end
+        end,
+      })
+    '';
+
     vimAlias = true;
   };
 }
